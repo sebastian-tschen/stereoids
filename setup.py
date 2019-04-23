70c94230de9d10cfb723dd06fed16778a2c142f2
Sebastian Behrens <sebastian.tschen.behrens@gmail.com>
2019-04-23 11:23:01 +0200

    -create pypi project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+REQUIREMENTS = [
+    'opencv-contrib-python',
+    'numpy',
+]
+
+
+setup(
+    name='stereoids',
+    version='0.0.1',
+    packages=['detector', 'corrector', 'stereoids'],
+    url='https://github.com/sebastian-tschen/stereoids',
+    license='MIT',
+    author='Sebastian Behrens',
+    author_email='sebastian.tschen.behrens@gmail.com',
+    description='a simple library for 3d positioning markers via stereo vision',
+    install_requires=REQUIREMENTS,
+)
